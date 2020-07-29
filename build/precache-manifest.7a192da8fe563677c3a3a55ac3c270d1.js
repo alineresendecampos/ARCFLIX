9f26def562f60414e3d89897c2e85d9e1210cf4b
Aline Resende Campos <alineresendecampos@gmail.com>
2020-07-29 09:58:14 +0100

    translated some text and run build for deployment

---
+++ build/precache-manifest.7a192da8fe563677c3a3a55ac3c270d1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd1a85d1b66bd7cdc569d6430b54ff28",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7a17909b9c8d7fa0c1c6",
+    "url": "/static/css/main.ee223eb7.chunk.css"
+  },
+  {
+    "revision": "6f9de1833d0b21d41b2f",
+    "url": "/static/js/2.df8311b0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.df8311b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a17909b9c8d7fa0c1c6",
+    "url": "/static/js/main.54ff3f52.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "7df29cf7ff708b6fc07cdf92b739ba38",
+    "url": "/static/media/Logo.7df29cf7.png"
+  }
+]);
